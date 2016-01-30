ee0c96310a0b23e83e55290c7d5996666b82d3ac
Nicklas Ahlskog <kivi@kivibot.fi>
2016-01-30 18:00:22 +0200

    Water

--- defaulttileset.lua
+++ defaulttileset.lua
@@ -4,5 +4,5 @@ ti = require "tileinfo"
 return TileSet("graphics/tileset.png", 24, 24, {
     ti.TileInfo(".", 0, 0),
     ti.TileInfo("#", 8, 3, true),
-    ti.AnimatedTileInfo("w", {{x=7,y=0}})
+    ti.AnimatedTileInfo("w", {{x=7,y=0}, {x=3,y=4}, {x=4,y=4}, {x=5,y=4}})
 })
