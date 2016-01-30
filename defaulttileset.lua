eb482d85ebbdd366b5f52834cd3c1dbc1dc537ec
Juhani Imberg <juhani@imberg.fi>
2016-01-30 13:36:32 +0200

    updated graphics indexes

--- defaulttileset.lua
+++ defaulttileset.lua
@@ -3,6 +3,6 @@ ti = require "tileinfo"
 
 return TileSet("graphics/tileset.png", 24, 24, {
     ti.TileInfo(".", 0, 0),
-    ti.TileInfo("#", 0, 1, true),
-    ti.AnimatedTileInfo("w", {{x=2,y=1}})
+    ti.TileInfo("#", 8, 3, true),
+    ti.AnimatedTileInfo("w", {{x=7,y=0}})
 })
