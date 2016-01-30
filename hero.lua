053660d36a7a24e736a02fcd3719cb9b6312333c
Juhani Imberg <juhani@imberg.fi>
2016-01-30 15:58:49 +0200

    hero moving animation in game

--- hero.lua
+++ hero.lua
@@ -5,13 +5,21 @@ CollidingEntity = require "collidingentity"
 Hero = Class{
     __includes = CollidingEntity,
     init = function(self, pos)
-        Entity.init(self, pos, 16, 16)
+        Entity.init(self, pos, 18, 18)
+        self.image = love.graphics.newImage("graphics/hero1.png")
+        self.quad = love.graphics.newQuad(0, 0, 18, 18, self.image:getWidth(), self.image:getHeight())
         self.joystick = nil
+        self.flipped = false
+        self.image_range = {1, 5}
     end,
     setJoystick = function(self, joystick)
         self.joystick = joystick
     end,
     update = function(self, dt, collfn)
+        local time = love.timer.getTime()
+        local image_ind = (math.floor(4*time)) % (self.image_range[2] - self.image_range[1]) + self.image_range[1]
+        self.quad:setViewport(image_ind * 18, 0, 18, 18)
+
         if not self.joystick then
             return
         end
@@ -23,20 +31,32 @@ Hero = Class{
         x_axis, y_axis = self.joystick:getAxes()
         axis = vector(x_axis, y_axis)
 
-
-
         if axis:len() > 0.1 then
             self:move(axis, dt, collfn)
+
+            if math.abs(axis.x) > math.abs(axis.y) then
+                self.image_range = {2, 6}
+                self.flipped = axis.x < 0
+            else
+                self.flipped = false
+                if axis.y < 0 then
+                    self.image_range = {6, 10}
+                else
+                    self.image_range = {10, 14}
+                end
+            end
         end
 
     end,
     draw = function(self)
-        if self.carrying then
-            love.graphics.setColor(200, 0, 0)
-        else
-            love.graphics.setColor(255, 0, 0)
-        end
-        love.graphics.rectangle("fill", self.pos.x, self.pos.y, self.width, self.height)
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.draw(self.image,
+                           self.quad,
+                           math.floor(self.pos.x + 0.5 - (self.flipped and -18 or 0)),
+                           math.floor(self.pos.y + 0.5),
+                           0,
+                           (self.flipped and -1 or 1),
+                           1)
     end
 }
 
