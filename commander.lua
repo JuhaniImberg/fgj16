73cf1e96df141a50bdbe83f126d2ca283937ac31
Juhani Imberg <juhani@imberg.fi>
2016-01-30 15:15:55 +0200

    commander stub

---
+++ commander.lua
@@ -0,0 +1,43 @@
+vector = require "hump.vector"
+Class = require "hump.class"
+
+
+Commander = Class{
+    init = function(self, pos)
+        self.pos = pos
+        self.width = 24
+        self.height = 24
+        self.joystick = nil
+        self.speed = 200
+    end,
+    setJoystick = function(self, joystick)
+        self.joystick = joystick
+    end,
+    update = function(self, dt, game)
+        if not self.joystick then
+            return
+        end
+
+        local x_axis, y_axis = self.joystick:getAxes()
+        local axis = vector(x_axis, y_axis)
+        if axis:len() > 0.1 then
+            self.pos = self.pos + (dt * self.speed * axis)
+            self.pos.x = math.max(0, math.min(24 * 52, self.pos.x))
+            self.pos.y = math.max(0, math.min(24 * 29, self.pos.y))
+        end
+    end,
+    draw = function(self)
+        love.graphics.setColor(0, 0, 0)
+        local rpos = self.pos:clone()
+        rpos.x = math.floor(rpos.x / 24 + 0.5) * 24
+        rpos.y = math.floor(rpos.y / 24 + 0.5) * 24
+        love.graphics.line(rpos.x, rpos.y,
+                           rpos.x + self.width, rpos.y,
+                           rpos.x + self.width, rpos.y + self.height,
+                           rpos.x, rpos.y + self.height,
+                           rpos.x, rpos.y)
+    end
+
+}
+
+return Commander
--- game.lua
+++ game.lua
@@ -8,6 +8,7 @@ item = require "item"
 Unit = require "unit"
 MtDoom = require "mtdoom"
 HQ = require "hq"
+Commander = require "commander"
 
 
 local game = {}
@@ -58,6 +59,7 @@ function game:init()
     table.insert(self.entities, HQ(vector(24, 28 * 24)))
 
     self.hero = Hero(vector(48, 48))
+    self.commander = Commander(vector(48, 27 * 24))
     table.insert(self.entities, self.hero)
 end
 
@@ -68,8 +70,8 @@ end
 
 function game.addRandomItem(self)
     local ntype = item.itemtypes[math.random(#item.itemtypes)]
-    local nitem = item.Item(vector(math.random(0, 53) * 24,
-                                   math.random(0, 30) * 24),
+    local nitem = item.Item(vector(math.random(1, 51) * 24,
+                                   math.random(1, 29) * 24),
                             ntype)
     table.insert(self.items, nitem)
 end
@@ -79,6 +81,7 @@ function game:update(dt)
     for i, item in ipairs(self.items) do
         item:update(dt, self.entities)
     end
+    self.commander:update(dt, self)
     for i, entity in ipairs(self.entities) do
         entity:update(dt, helpers.bind(self.tm, 'collides'), self)
     end
@@ -89,6 +92,7 @@ function game:draw()
     for i, nitem in ipairs(self.items) do
         nitem:draw()
     end
+    self.commander:draw()
     for i, entity in ipairs(self.entities) do
         entity:draw()
     end
@@ -96,6 +100,7 @@ end
 
 function game:joystickadded(joystick)
     self.hero:setJoystick(joystick)
+    -- self.commander:setJoystick(joystick)
 end
 
 return game
