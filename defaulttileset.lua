5afc0908bce06073dd4c0471df93867d89acbd28
Juhani Imberg <juhani@imberg.fi>
2016-01-30 13:00:30 +0200

    updated defaulttileset to use the new tileset

--- defaulttileset.lua
+++ defaulttileset.lua
@@ -1,10 +1,8 @@
 TileSet = require "tileset"
 ti = require "tileinfo"
 
-return TileSet("tileset.png", 24, 24, {
+return TileSet("graphics/tileset.png", 24, 24, {
     ti.TileInfo(".", 0, 0),
-    ti.TileInfo("#", 1, 0, true),
-    ti.TileInfo("%", 0, 1),
-    ti.TileInfo("?", 1, 1),
-    ti.AnimatedTileInfo("w", {{x=0,y=2},{x=0,y=3},{x=0,y=4}})
+    ti.TileInfo("#", 0, 1, true),
+    ti.AnimatedTileInfo("w", {{x=2,y=1}})
 })
